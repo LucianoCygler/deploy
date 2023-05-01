5c9e26558f371360d5275d13c88232e6aa31ccfb
Luciano <lucianocygler@outlook.com>
2023-04-30 23:28:09 -0300

    add info env

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const Pokemon = require("../models/Pokemon");
 const pokemonsRouter = require("./pokemonsRouter");
 const typesRouter = require("./typesRouter");
 const router = Router();
