8b505aefa1b89bf4ba8b84e7bc08ba5e678fcac1
Luciano <lucianocygler@outlook.com>
2023-05-02 20:56:57 -0300

    New paginated

--- client/src/components/paginado/paginado.jsx
+++ client/src/components/paginado/paginado.jsx
@@ -7,15 +7,56 @@ export default function Paginado({
   paginated,
   currentPage,
 }) {
+  const totalPages = Math.ceil(allPokemons / pokemonsPerPage);
+  const pageLimit = 5;
+  const halfPageLimit = Math.floor(pageLimit / 2);
+  let startPage = currentPage - halfPageLimit;
+  let endPage = currentPage + halfPageLimit;
+
+  if (startPage < 1) {
+    startPage = 1;
+    endPage = startPage + pageLimit - 1;
+  }
+
+  if (endPage > totalPages) {
+    endPage = totalPages;
+    startPage = endPage - pageLimit + 1;
+  }
+  if (totalPages < pageLimit) {
+    startPage = 1;
+    endPage = totalPages;
+  }
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(allPokemons / pokemonsPerPage); i++) {
+  for (let i = startPage; i <= endPage; i++) {
     pageNumbers.push(i);
   }
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      paginated(currentPage + 1);
+    }
+  };
+  const handleBack = () => {
+    if (currentPage > 1) {
+      paginated(currentPage - 1);
+    }
+  };
+  const handleLast = () => {
+    paginated(totalPages);
+  };
+  const handleFirst = () => {
+    paginated(1);
+  };
   return (
     <nav className={styles.container}>
-      <ul className={styles.paginadoContainer}>
-        {pageNumbers &&
-          pageNumbers.map((number) => (
+      {totalPages > 1 && (
+        <ul className={styles.paginadoContainer}>
+          <li className={styles.pagina} onClick={handleFirst}>
+            First
+          </li>
+          <li className={styles.pagina} onClick={handleBack}>
+            Back
+          </li>
+          {pageNumbers.map((number) => (
             <li
               className={`${styles.pagina} ${
                 currentPage === number ? styles.active : ""
@@ -25,7 +66,14 @@ export default function Paginado({
               <a>{number}</a>
             </li>
           ))}
-      </ul>
+          <li className={styles.pagina} onClick={handleNext}>
+            Next
+          </li>
+          <li className={styles.pagina} onClick={handleLast}>
+            Last
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
